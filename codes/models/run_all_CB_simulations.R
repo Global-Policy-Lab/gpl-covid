124ae0b68cc516f83c7d63829bb5ca7f9d80f4e6
Kendon Bell <kmb56@berkeley.edu>
2020-03-24 13:05:19 +1300

    update jhu scripts

--- codes/models/run_all_CB_simulations.R
+++ codes/models/run_all_CB_simulations.R
@@ -1,18 +1,29 @@
 # Full script to run R code for projection:
-library(tidyverse)
-library(lfe)
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(lfe))
 set.seed(718)
-times = 1000
+times = 2
 # source("codes/models/calculate_gamma_from_china_and_korea.R")
-gamma = readr::read_csv("models/gamma_est.csv") %>% 
+gamma = readr::read_csv("models/gamma_est.csv",
+                        col_types = 
+                          cols(
+                            adm0_name = col_character(),
+                            gamma_est = col_double()
+                          )) %>% 
   filter(adm0_name %in% c("CHN", "KOR")) %>% 
   pull(gamma_est) %>% 
   mean()
 
 dir.create("data/post_processing", recursive=TRUE, showWarnings=FALSE)
+message("Running France projection.")
 source("codes/models/FRA_create_CBs.R")
+message("Running Iran projection.")
 source("codes/models/IRN_create_CBs.R")
+message("Running South Korea projection.")
 source("codes/models/KOR_create_CBs.R")
+message("Running USA projection.")
 source("codes/models/USA_create_CBs.R")
+message("Running China projection.")
 source("codes/models/CHN_create_CBs.R")
+message("Running Italy projection.")
 source("codes/models/ITA_create_CBs.R")
