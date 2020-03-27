46a761990bfecb0336b7001ffd95134405a3a5ee
dpa9694 <danielpaulallen@gmail.com>
2020-03-27 13:49:48 -0700

    Update codes/pop.py
    improve error handling
    
    Co-Authored-By: Ian Bolliger <bolliger@berkeley.edu>

--- codes/pop.py
+++ codes/pop.py
@@ -36,7 +36,7 @@ def merge_policies_with_population_on_level(policies, adm_level, country_code, e
         adm_level (int): Adm-level of requested populations.
         country_code (str): Three-letter country code of requested populations
     Kwargs:
-       errors (str): Error-handling behavior. Options are "raise", "ignore", "warn", and "error" (default)
+       errors (str): Error-handling behavior. Options are "raise" (default), "ignore", and "warn"
 
     Returns:
         pandas.DataFrame: `policies` with a new column, "adm_{`adm_level`}_pop"
