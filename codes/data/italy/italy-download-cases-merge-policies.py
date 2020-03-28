66a530edf24b5eef694e97b594970134b9eca81b
Daniel Allen <danielpaulallen@gmail.com>
2020-03-28 16:08:33 -0700

    abstraction within italy script

--- codes/data/italy/italy-download-cases-merge-policies.py
+++ codes/data/italy/italy-download-cases-merge-policies.py
@@ -21,7 +21,6 @@ path_template = cutil.DATA_PROCESSED / '[country]_processed.csv'
 dir_italy_raw = cutil.DATA_RAW / 'italy'
 dir_italy_interim = cutil.DATA_INTERIM / 'italy'
 
-
 # Inputs
 # CSV form of policies Google sheet
 path_italy_policies = dir_italy_raw / 'ITA_policy_data_sources.csv'
@@ -42,7 +41,6 @@ path_processed_province = cutil.DATA_PROCESSED / 'adm2' / 'ITA_processed.csv'
 # ## Download and read raw data from Github
 
 # #### Read inputs
-
 # Columns in template (i.e. columns allowed in output)
 template_cols = set(pd.read_csv(path_template).columns)
 
@@ -57,12 +55,10 @@ policies_full = pd.read_csv(path_italy_policies)
 adm2_cases.to_csv(path_italy_raw_province, index=False)
 adm1_cases.to_csv(path_italy_raw_region, index=False)
 
-
 # ## Translate and clean health data
 
 # ###### Settings
 # Affixes defined in `data_dictionary.gsheet`
-
 cumulative_prefix = 'cum_'
 imputed_suffix = '_imputed'
 popweighted_suffix = '_popwt'
@@ -70,7 +66,6 @@ optional_suffix = '_opt'
 
 
 # ### Translate field names from Italian to project naming scheme
-
 # Column names based on table descriptions here: https://github.com/pcm-dpc/COVID-19
 replace_dict = {
     'data':'date',
@@ -97,7 +92,6 @@ replace_dict = {
 adm2_cases = adm2_cases.rename(columns=replace_dict)
 adm1_cases = adm1_cases.rename(columns=replace_dict)
 
-
 # Clean date column
 def extract_date_from_datetime(dates):
     return pd.to_datetime(dates.str[:10])
@@ -105,68 +99,55 @@ def extract_date_from_datetime(dates):
 adm2_cases['date'] = extract_date_from_datetime(adm2_cases['date'])
 adm1_cases['date'] = extract_date_from_datetime(adm1_cases['date'])
 
-
 # Clean lat-lon coordinates
 adm2_cases.loc[:,['lat','lon']] = adm2_cases.loc[:,['lat','lon']].replace(0, np.nan)
 assert adm1_cases['lat'].isnull().sum() == 0
 assert adm1_cases['lon'].isnull().sum() == 0
 
-
 # Clean unknown province names
 # "In fase di definizione/aggiornamento" translates to "Being defined / updated". These observations are dropped from the final output
-adm2_cases['adm2_name'] = adm2_cases['adm2_name'].replace(
-    'In fase di definizione/aggiornamento', 'Unknown')
+adm2_cases['adm2_name'] = adm2_cases['adm2_name'].replace('In fase di definizione/aggiornamento', 'Unknown')
 
-# Drop unnecessary column
-
-# In[ ]:
-
+# Drop extraneous columns
 adm1_cases = adm1_cases.drop(columns=[col for col in adm1_cases.columns if col not in replace_dict.values()])
 adm2_cases = adm2_cases.drop(columns=[col for col in adm2_cases.columns if col not in replace_dict.values()])
 
 # Impute cumulative confirmed cases at `adm2` level on the first day of the dataset (2/24/2020) from `adm1`
+def impute_day1_adm2_cases(adm1_cases, adm2_cases):
+	# Adm1 totals computed by grouping on Adm1 in the Adm2 dataset
+	adm1_cases_from_provinces = adm2_cases.groupby(['date', 'adm1_name'])['cum_confirmed_cases'].sum()
 
-# In[ ]:
+	# Compute cumulative cases in the Adm1 dataset by mapping to totals from Adm2 dataset
+	def get_province_total(region_row):
+	    return adm1_cases_from_provinces.loc[region_row['date'], region_row['adm1_name']]
 
+	# This sum should match each adm1-level total for each day, except the first day in the dataset
+	adm1_province_totals = adm1_cases.apply(get_province_total, axis=1)
 
-# Adm1 totals computed by grouping on Adm1 in the Adm2 dataset
-adm1_cases_from_provinces = adm2_cases.groupby(['date', 'adm1_name'])['cum_confirmed_cases'].sum()
+	# Compute DataFrame mapping adm1 names to first-day case totals that are missing in `adm2_cases`
+	day1_cases = adm1_cases[adm1_province_totals != adm1_cases['cum_confirmed_cases']][['adm1_name', 'cum_confirmed_cases']].set_index('adm1_name')
 
-# Compute cumulative cases in the Adm1 dataset by mapping to totals from Adm2 dataset
-def get_province_total(region_row):
-    return adm1_cases_from_provinces.loc[region_row['date'], region_row['adm1_name']]
+	# Mask to fill in adm2 rows with missing day 1 cum_confirmed_cases
+	replace_day1_mask = (
+	    (adm2_cases['adm1_name'].isin(day1_cases.index)) & 
+	    (adm2_cases['date'] == '2020-02-24') & 
+	    (adm2_cases['adm2_name'] == 'Unknown')
+	)
 
-# This sum should match each adm1-level total for each day, except the first day in the dataset
-adm1_cases['cum_confirmed_cases_prov'] = adm1_cases.apply(get_province_total, axis=1)
+	# Set cum_confirmed_cases of "Unknown" adm2 rows to each corresponding adm1 total on day 1
+	adm2_cases.loc[replace_day1_mask, 'cum_confirmed_cases'] = adm2_cases.loc[replace_day1_mask, 'adm1_name'].apply(lambda x: day1_cases.loc[x])
 
-# Compute DataFrame mapping adm1 names to first-day case totals that are missing in `adm2_cases`
-day1_cases = adm1_cases[adm1_cases['cum_confirmed_cases_prov'] != adm1_cases['cum_confirmed_cases']][['adm1_name', 'cum_confirmed_cases']].set_index('adm1_name')
+	# Check that all regions with positive cases on day 1 are accounted for
+	adm2_cases[(adm2_cases['adm1_name'].isin(day1_cases.index)) & (adm2_cases['date'] == '2020-02-24') & (adm2_cases['adm2_name'] == 'Unknown')]
 
-# Mask to fill in adm2 rows with missing day 1 cum_confirmed_cases
-replace_day1_mask = (
-    (adm2_cases['adm1_name'].isin(day1_cases.index)) & 
-    (adm2_cases['date'] == '2020-02-24') & 
-    (adm2_cases['adm2_name'] == 'Unknown')
-)
+	return adm2_cases
 
-# Set cum_confirmed_cases of "Unknown" adm2 rows to each corresponding adm1 total on day 1
-adm2_cases.loc[replace_day1_mask, 'cum_confirmed_cases'] = adm2_cases.loc[replace_day1_mask, 'adm1_name'].apply(lambda x: day1_cases.loc[x])
-
-# Remove helper col
-adm1_cases = adm1_cases.drop(columns=['cum_confirmed_cases_prov'])
-
-# Check that all regions with positive cases on day 1 are accounted for
-adm2_cases[(adm2_cases['adm1_name'].isin(day1_cases.index)) & (adm2_cases['date'] == '2020-02-24') & (adm2_cases['adm2_name'] == 'Unknown')]
-
+adm2_cases = impute_day1_adm2_cases(adm1_cases, adm2_cases)
 
 # ###### Check data limitations
 # Go to https://github.com/pcm-dpc/COVID-19 and check "Avvisi" for any documented data issues
 
 # #### Fill known missing cumulative totals as nulls
-
-# In[ ]:
-
-
 # Information on missingness gathered from GitHub "Avvisi" section
 adm1_days_missing = [
     ("2020-03-10", "Lombardia"),
@@ -209,23 +190,10 @@ for date, adm2 in adm2_days_missing:
     adm2_cases = fill_missing_as_null(adm2_cases, date, adm2, 'adm2_name')
 
 
-# In[ ]:
-
-
-adm1_cases[adm1_cases['cum_confirmed_cases'].isnull()]
-
-
-# In[ ]:
-
-
 adm2_cases[adm2_cases['cum_confirmed_cases'].isnull()].sample(5)
 
 
 # ### Impute values in cases where cumulative counts rise and then fall
-
-# In[ ]:
-
-
 def convert_non_monotonic_to_nan(array):
     """Converts a numpy array to a monotonically increasing one.
     Args:
@@ -324,20 +292,18 @@ def impute_cumulative_df(df, src_col, dst_col, groupby_col):
     return df
 
 
+def impute_each_cumulative_column(adm_cases, adm_id_col):
+	# Impute any cumulative totals in adm1
+	adm_cases_cum_cols = [col for col in adm_cases.columns if col.startswith(cumulative_prefix)]
+	for src_col in adm_cases_cum_cols:
+	    dst_col = src_col + imputed_suffix
+	    adm_cases = impute_cumulative_df(adm_cases, src_col, dst_col, adm_id_col)
+
+	return adm_cases
+
 # #### Impute all cumulative totals in imputed column, fill as null where cumulative totals fall in source column
-# Impute adm2 cumulative total (just one)
-adm2_cases = impute_cumulative_df(
-    adm2_cases, 
-    cumulative_prefix + 'confirmed_cases', 
-    cumulative_prefix + 'confirmed_cases' + imputed_suffix, 
-    'adm2_id')
-
-# Impute any cumulative totals in adm1
-adm1_cases_cum_cols = [col for col in adm1_cases.columns if 'cum_' in col]
-for src_col in adm1_cases_cum_cols:
-    dst_col = src_col + imputed_suffix
-    adm1_cases = impute_cumulative_df(adm1_cases, src_col, dst_col, 'adm1_name')
-
+adm1_cases = impute_each_cumulative_column(adm1_cases, 'adm1_name')
+adm2_cases = impute_each_cumulative_column(adm2_cases, 'adm2_id')
 
 # #### Save processed health data to `interim` folder
 path_italy_interim_province.parent.mkdir(parents=True, exist_ok=True)
