91c581beaec549eac3176b321c11cc28a705b6f2
Ian Bolliger <bolliger@berkeley.edu>
2020-04-05 08:34:32 -0700

    add readme test

---
+++ tests/tests.py
@@ -0,0 +1,11 @@
+
+def test_readme():
+    with open("README.md", "r") as f:
+        readme = f.read()
+    with open("run", "r") as f:
+        for l in f:
+            for prog in ["python", "Rscript", "stata"]:
+                # skip quality check, which is not in readme
+                if l.startswith(prog) and "quality-check-processed-datasets" not in l:
+                    tocomp = l.rstrip("\n")
+                    assert tocomp in readme, tocomp
