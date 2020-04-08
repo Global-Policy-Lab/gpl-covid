3ae5be9b9e3760e5b293cebe20e2baa059efd076
Ian Bolliger <bolliger@berkeley.edu>
2020-04-08 15:05:05 -0700

    black

--- codes/models/epi.py
+++ codes/models/epi.py
@@ -13,7 +13,7 @@ def init_reg_arr(n_samples, n_reg, n_policies, *args):
     dims = [n_samples]
     for a in args:
         dims.append(a)
-    dims += [n_reg, n_policies+1]
+    dims += [n_reg, n_policies + 1]
     estimates = np.empty(dims, dtype=np.float32)
     estimates.fill(np.nan)
     return estimates
@@ -27,7 +27,9 @@ def adjust_timescales_from_daily(tsteps_per_day, *args):
     return [i / tsteps_per_day for i in args]
 
 
-def init_policy_dummies(effects, starts, ends, n_samples, n_steps, steps_per_day, seed=0):
+def init_policy_dummies(
+    effects, starts, ends, n_samples, n_steps, steps_per_day, seed=0
+):
     np.random.seed(seed)
     n_effects = len(effects)
     dates = np.empty((n_samples, n_effects), dtype=np.int16)
@@ -35,16 +37,18 @@ def init_policy_dummies(effects, starts, ends, n_samples, n_steps, steps_per_day
         dates[:, ix] = np.random.randint(starts[ix], ends[ix], n_samples)
 
     # drop any with complete collinearity of policies
-    valid = np.apply_along_axis(lambda x: len(x)==dates.shape[1], 1, np.unique(dates,axis=1))
+    valid = np.apply_along_axis(
+        lambda x: len(x) == dates.shape[1], 1, np.unique(dates, axis=1)
+    )
     dates = dates[valid]
     n_samples = dates.shape[0]
-    
+
     # create policy dummy array
     out = np.empty((n_samples, n_effects, n_steps), dtype=np.int16)
     comp = np.repeat(np.arange(out.shape[-1])[:, np.newaxis], dates.shape[1], axis=1)
     for s in range(out.shape[0]):
         out[s] = np.where(comp >= dates[s] * steps_per_day, 1, 0).T
-    return out.swapaxes(1,2)
+    return out.swapaxes(1, 2)
 
 
 def get_beta_SEIR(growth_rate_deterministic, gamma_to_test, sigma_to_test):
@@ -53,9 +57,13 @@ def get_beta_SEIR(growth_rate_deterministic, gamma_to_test, sigma_to_test):
     rate, sigma, and gamma. This calculates that based on the deterministic gamma and 
     sigmas.
     """
-    gammas = np.repeat(np.array(gamma_to_test)[:,np.newaxis], len(sigma_to_test), axis=1)
-    sigmas = np.repeat(np.array(sigma_to_test)[np.newaxis,:], len(gamma_to_test), axis=0)
-    g = growth_rate_deterministic[...,np.newaxis,np.newaxis]
+    gammas = np.repeat(
+        np.array(gamma_to_test)[:, np.newaxis], len(sigma_to_test), axis=1
+    )
+    sigmas = np.repeat(
+        np.array(sigma_to_test)[np.newaxis, :], len(gamma_to_test), axis=0
+    )
+    g = growth_rate_deterministic[..., np.newaxis, np.newaxis]
     return (g + gammas) * (g + sigmas) / sigmas
 
 
@@ -74,45 +82,37 @@ def get_stochastic_params(
         beta_noise = np.random.normal(0, beta_noise_sd, beta_det.shape[:2])
     else:
         beta_noise = np.zeros(beta_det.shape[:2])
-    beta_noise = beta_noise[...,np.newaxis]
-    
+    beta_noise = beta_noise[..., np.newaxis]
+
     if gamma_noise_on:
         gamma_noise = np.random.normal(0, gamma_noise_sd, beta_det.shape[:2])
     else:
         gamma_noise = np.zeros(beta_det.shape[:2])
-    this_gamma = gamma_noise[...,np.newaxis] + np.array(gamma_to_test)
-    
+    this_gamma = gamma_noise[..., np.newaxis] + np.array(gamma_to_test)
+
     if sigma_to_test is not None:
-        assert len(beta_det.shape)==4
-        beta_noise = beta_noise[...,np.newaxis]
+        assert len(beta_det.shape) == 4
+        beta_noise = beta_noise[..., np.newaxis]
         if sigma_noise_on:
             sigma_noise = np.random.normal(0, sigma_noise_sd, beta_det.shape[:2])
         else:
             sigma_noise = np.zeros(beta_det.shape[:2])
-        this_sigma = sigma_noise[...,np.newaxis] + np.array(sigma_to_test)
+        this_sigma = sigma_noise[..., np.newaxis] + np.array(sigma_to_test)
         sigma_out = [this_sigma]
     else:
-        assert len(beta_det.shape)==3
+        assert len(beta_det.shape) == 3
         sigma_out = []
-        
+
     this_beta = beta_noise + beta_det
     out_vals = [this_beta, this_gamma] + sigma_out
-    
+
     # make sure none are non-positive
     out_vals = [np.where(i > 0, i, 0) for i in out_vals]
-    
+
     return out_vals
 
 
-def run_SEIR(
-    n_steps,
-    E0,
-    I0,
-    R0,
-    beta,
-    gamma,
-    sigma
-):
+def run_SEIR(n_steps, E0, I0, R0, beta, gamma, sigma):
     """
     Simulate SEIR model using forward euler integration. All states are defined as 
     fractions of a population.
@@ -131,17 +131,17 @@ def run_SEIR(
     S, E, I, R : :class:`numpy.ndarray`
         State space of the model at all timesteps
     """
-    
+
     S, E, I, R = init_state_arrays(n_steps, 4)
-    
+
     # initial conditions
     R[0] = R0
     I[0] = I0
     E[0] = E0
     S[0] = 1 - I[0] - R[0] - E[0]
-    
+
     for i in range(1, n_steps):
-        new_exposed = beta[i - 1]*S[i - 1]*I[i - 1]
+        new_exposed = beta[i - 1] * S[i - 1] * I[i - 1]
         new_infected = sigma[i - 1] * E[i - 1]
         new_removed = gamma[i - 1] * I[i - 1]
 
@@ -149,26 +149,39 @@ def run_SEIR(
         E[i] = E[i - 1] + new_exposed - new_infected
         I[i] = I[i - 1] + new_infected - new_removed
         R[i] = 1 - S[i] - E[i] - I[i]
-        
+
     return S, E, I, R
 
 
-def get_case_df(mod_type, state_arrs, policy_arr, t, steps_per_output, pop, param_arr_dict=None, debug=False):
+def get_case_df(
+    mod_type,
+    state_arrs,
+    policy_arr,
+    t,
+    steps_per_output,
+    pop,
+    param_arr_dict=None,
+    debug=False,
+):
     assert len(mod_type) == len(state_arrs)
-    state_dict = {i: state_arrs[ix] for ix,i in enumerate(mod_type)}
-    
+    state_dict = {i: state_arrs[ix] for ix, i in enumerate(mod_type)}
+
     if debug:
         assert param_arr_dict is not None
         state_dict = {**state_dict, **param_arr_dict}
     index = pd.Index(t.astype(int), name="day")
-    df = pd.DataFrame(state_dict, index=index).iloc[::steps_per_output,:]
-    df.loc[:,list(mod_type)] *= pop
+    df = pd.DataFrame(state_dict, index=index).iloc[::steps_per_output, :]
+    df.loc[:, list(mod_type)] *= pop
     df["IR"] = df["I"] + df["R"]
-    if mod_type=="SEIR":
+    if mod_type == "SEIR":
         df["EI"] = df["E"] + df["I"]
         df["EIR"] = df["EI"] + df["R"]
-        
-    policy_df = pd.DataFrame(policy_arr, index = index, columns = [f"p{ix+1}" for ix in range(policy_arr.shape[1])]).iloc[::steps_per_output,:]
+
+    policy_df = pd.DataFrame(
+        policy_arr,
+        index=index,
+        columns=[f"p{ix+1}" for ix in range(policy_arr.shape[1])],
+    ).iloc[::steps_per_output, :]
     df = pd.concat((df, policy_df), axis=1)
     return df
 
@@ -196,10 +209,8 @@ def run_regressions(cases, LHS_vars, policy_vars, min_cases):
 def res_arr_to_ds(estimates, reg_to_run, policies_to_include, param_kwargs, **attrs):
     n_samples = estimates.shape[0]
     ## Convert array to Dataset
-    coords = OrderedDict(
-        sample=range(n_samples)
-    )
-    for p,v in param_kwargs.items():
+    coords = OrderedDict(sample=range(n_samples))
+    for p, v in param_kwargs.items():
         coords[p] = v
 
     coords["case_type"] = reg_to_run
--- codes/plotting/sims.py
+++ codes/plotting/sims.py
@@ -3,25 +3,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 import warnings
 
+
 def facet_hist(estimates, case_type, coef, **kwargs):
     true_vals = {
         "Intercept": "no_policy_growth_rate",
         "p1": "p1_effect",
         "p2": "p2_effect",
-        "p3": "p3_effect"
+        "p3": "p3_effect",
     }
     g = xr.plot.FacetGrid(estimates.sel(case_type=case_type), **kwargs, sharex=True)
-    
+
     def nowarn_hist(*args, **kwargs):
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             return plt.hist(*args, **kwargs)
-    g.map(nowarn_hist,coef)
-    g.map(lambda x: plt.axvline(np.nanmean(x), color="r", linestyle="--", label="Mean estimate"),coef)
+
+    g.map(nowarn_hist, coef)
+    g.map(
+        lambda x: plt.axvline(
+            np.nanmean(x), color="r", linestyle="--", label="Mean estimate"
+        ),
+        coef,
+    )
     for ax in g.axes.flat:
-        ax.axvline(estimates.attrs[true_vals[coef]], color='k', label="True value")
+        ax.axvline(estimates.attrs[true_vals[coef]], color="k", label="True value")
     g.axes.flat[0].legend()
     g.set_xlabels("")
     g.set_titles("$\{coord}$ = {value}")
-    g.fig.suptitle(f"LHS: {case_type}; variable: {coef}", va="bottom", y=.99)
+    g.fig.suptitle(f"LHS: {case_type}; variable: {coef}", va="bottom", y=0.99)
     return g
