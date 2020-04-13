d65edfbca637c71a32a35d24bae9b9fa1007aa2e
Ian Bolliger <bolliger@berkeley.edu>
2020-04-13 13:35:04 -0700

    don't test data folder

--- tests/tests.py
+++ tests/tests.py
@@ -18,36 +18,26 @@ def test_readme():
 
 
 def test_pipeline(tmp_path):
-    copytree("data", tmp_path / "data")
-    copytree("models", tmp_path / "models")
-
-    # don't check raw data
-    rmtree(tmp_path / "data" / "raw")
+    tmp_model_path = tmp_path / "models"
+    copytree("models", tmp_model_path)
 
     # run pipeline
     cmd = shlex.split("bash run --nostata --nocensus --num-proj 2")
     process = subprocess.run(cmd, check=True)
 
-    # check that interim/processed/post_processing data and models match
-    with open("tests/ignore_comparison.txt", "r") as f:
-        to_skip = f.readlines()
-    to_skip = [Path(tmp_path) / p.strip() for p in to_skip]
-    print(to_skip)
     bad_files = []
-    for d in ["data", "models"]:
-        path = tmp_path / d
-        len_path = len(tmp_path.parts)
-        for p in path.rglob("*"):
-            if p.suffix == ".csv" and p not in to_skip:
-                other_file = Path("").joinpath(*p.parts[len_path:])
-                try:
-                    pd.testing.assert_frame_equal(
-                        pd.read_csv(p), pd.read_csv(other_file)
-                    )
-                except UnicodeDecodeError:
-                    pass
-                except:
-                    bad_files.append(str(other_file))
+    len_path = len(tmp_path.parts)
+    for p in tmp_model_path.rglob("*"):
+        if p.suffix == ".csv":
+            other_file = Path("").joinpath(*p.parts[len_path:])
+            try:
+                pd.testing.assert_frame_equal(
+                    pd.read_csv(p), pd.read_csv(other_file)
+                )
+            except UnicodeDecodeError:
+                pass
+            except:
+                bad_files.append(str(other_file))
     if len(bad_files) > 0:
         raise AssertionError(
             "The folowing files produced by this code do not match the version saved "
