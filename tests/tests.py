5ec6f021bb5671a20a8c1d996b28717704a3e8b6
Ian Bolliger <bolliger@berkeley.edu>
2020-04-13 10:45:09 -0700

    no more comparison files

--- tests/tests.py
+++ tests/tests.py
@@ -2,13 +2,31 @@ from pathlib import Path
 import pandas as pd
 import shlex
 import subprocess
+from shutil import copytree
 
 
-def test_pipeline():
+def test_pipeline(tmp_path):
+    copytree("data", tmp_path)
+    copytree("models", tmp_path)
     cmd = shlex.split("bash run --nostata --nocensus --num-proj 2")
     process = subprocess.run(cmd, check=True)
 
+    with open("tests/ignore_comparison.txt", "r") as f:
+        to_skip = f.readlines()
+    to_skip = [Path(tmp_path) / p for p in to_skip]
 
+    for d in ["data", "models"]:
+        path = tmp_path / d
+        len_path = len(tmp_path.parts)
+        for p in path.rglob("*"):
+            if p.suffix == ".csv" and p not in to_skip:
+                other_file = Path("").joinpath(*p.parts[len_path:])
+                try:
+                    pd.testing.assert_frame_equal(pd.read_csv(p), pd.read_csv(other_file))
+                except UnicodeDecodeError:
+                    pass
+                
+                
 def test_readme():
     with open("README.md", "r") as f:
         readme = f.read()
@@ -19,14 +37,3 @@ def test_readme():
                 if l.startswith(prog) and "quality-check-processed-datasets" not in l:
                     tocomp = l.rstrip("\n").split(" $")[0]
                     assert tocomp in readme, tocomp
-
-
-def test_no_data_change():
-    path = Path("tests/comparison_files")
-    for p in path.rglob("*"):
-        if p.is_file() and not any([l.startswith(".") for l in p.parts]):
-            other_file = Path("").joinpath(*p.parts[2:])
-            try:
-                pd.testing.assert_frame_equal(pd.read_csv(p), pd.read_csv(other_file))
-            except UnicodeDecodeError:
-                pass
