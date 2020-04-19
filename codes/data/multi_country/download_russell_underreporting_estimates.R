f1d62bc0f2a867fc94e912a0dda23fa6122f3a1b
Kendon Bell <kmb56@berkeley.edu>
2020-04-20 08:54:00 +1200

    update underreporting

--- codes/data/multi_country/download_russell_underreporting_estimates.R
+++ codes/data/multi_country/download_russell_underreporting_estimates.R
@@ -1,4 +1,4 @@
-library(tidyverse)
+suppressPackageStartupMessages(library(tidyverse))
 cfr_data <- read_rds(url("https://github.com/thimotei/CFR_calculation/blob/master/global_estimates/data/reportDataFinal.rds?raw=true", "rb"))
 
 cfr_data <- cfr_data %>% 
@@ -6,3 +6,4 @@ cfr_data <- cfr_data %>%
 
 dir.create("data/interim/multi_country/", showWarnings = FALSE)
 write_rds(cfr_data, "data/interim/multi_country/under_reporting.rds")
+write_csv(cfr_data, "data/interim/multi_country/under_reporting.csv")
