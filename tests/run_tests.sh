52030a482b25ed91a87f8608b8c26c2ee31a8be9
Ian Bolliger <bolliger@berkeley.edu>
2020-04-25 16:38:51 -0700

    stop using other action

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,6 +5,9 @@ set -e
 # check formatting
 black . --check
 
+# activate environment
+conda activate gpl-covid
+
 # make sure jupyter kernel exists
 python -m ipykernel install --user --name gpl-covid
 
