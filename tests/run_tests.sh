ddf1c8e0f9b3b601ab4a6d85351f82fa071aa4da
Ian Bolliger <bolliger@berkeley.edu>
2020-04-25 16:41:17 -0700

    try activating first

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,9 +5,6 @@ set -e
 # check formatting
 black . --check
 
-# activate environment
-conda activate gpl-covid
-
 # make sure jupyter kernel exists
 python -m ipykernel install --user --name gpl-covid
 
