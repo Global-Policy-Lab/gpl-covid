8df5040c40d1e2acd02dcf532a76552220269a8a
Ian Bolliger <bolliger@berkeley.edu>
2020-04-25 17:51:07 -0700

    fix no-stata tests

--- tests/tests.py
+++ tests/tests.py
@@ -43,7 +43,7 @@ def test_pipeline(tmp_path):
         + list(Path("results/source_data").glob("fig1*.csv"))
     )
     if not run_stata:
-        files_to_exclude += [
+        files_to_exclude += set([
             Path("results") / "source_data" / i
             for i in [
                 "Figure2_data.csv",
@@ -54,7 +54,7 @@ def test_pipeline(tmp_path):
                 "ExtendedDataFigure6.xlsx",
                 "ExtendedDataFigure10_e.csv",
             ]
-        ]
+        ])
 
     tmp_model_path = tmp_path / "models"
     tmp_results_path = tmp_path / "results" / "source_data"
