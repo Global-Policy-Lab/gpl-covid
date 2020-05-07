54ccfc9d479156309474355c41952b38538b8648
Daniel Allen <danielpaulallen@gmail.com>
2020-05-07 10:22:31 -0700

    update iran_cleaning.R to use IRN_policy_data_sources.csv

--- code/data/iran/iran_cleaning.R
+++ code/data/iran/iran_cleaning.R
@@ -1,20 +1,20 @@
 # --------------------------------------------------------
-# 
+#
 # Final cleaning and merge of Covid-19 data for Iran.
 #
-# 
-# NOTE ABOUT THE DATA: Iran has generally reported adm2 level new cases. 
+#
+# NOTE ABOUT THE DATA: Iran has generally reported adm2 level new cases.
 # However, on the dates of March 2 and 3, 2020, Iran only reported
 # national new cases, not broken down to the adm2 level. We will handle
 # this in two different options:
 # 1) Report as missing any adm2 outcomes for these two days.
 # 2) Disaggregate the national new cases on March 2 and 3 to
 #    the adm2 level, using March 1 new cases as adm2-weights.
-# 
-# 
+#
+#
 # Andy Hultgren, hultgren@berkeley.edu
 # 3/15/20
-# 
+#
 # --------------------------------------------------------
 
 
@@ -39,10 +39,10 @@ myThemeStuff <- theme(panel.background = element_rect(fill = NA),
 )
 
 # Set paths
-cases_in <- 'data/interim/iran/covid_iran_cases.csv' 
-policies_in <- 'data/interim/iran/covid_iran_policies.csv' 
-file_out <- 'data/interim/iran/IRN_interim.csv' 
-file_out_adm0 <- 'data/interim/iran/adm0/IRN_interim.csv' 
+cases_in <- 'data/interim/iran/covid_iran_cases.csv'
+policies_in <- 'data/raw/iran/IRN_policy_data_sources.csv'
+file_out <- 'data/interim/iran/IRN_interim.csv'
+file_out_adm0 <- 'data/interim/iran/adm0/IRN_interim.csv'
 dir.create('data/interim/iran/adm0', showWarnings=FALSE)
 
 ### !!! User defined !!!
@@ -65,15 +65,15 @@ cases.data$adm1_name <- NA
 cases.data$adm2_name <- NA
 
 cases.data$adm1_name <- as.numeric( sapply( 1:length(cases.data$variable), function(x) {
-  ifelse( (substr(cases.data$variable[x], nchar(cases.data$variable[x])-2, nchar(cases.data$variable[x])-2) == '_'), 
-          substr(cases.data$variable[x], nchar(cases.data$variable[x]), nchar(cases.data$variable[x])), 
-          NA) 
+  ifelse( (substr(cases.data$variable[x], nchar(cases.data$variable[x])-2, nchar(cases.data$variable[x])-2) == '_'),
+          substr(cases.data$variable[x], nchar(cases.data$variable[x]), nchar(cases.data$variable[x])),
+          NA)
 }))
 
 cases.data$adm2_name <- sapply( 1:length(cases.data$variable), function(x) {
-  ifelse( (substr(cases.data$variable[x], nchar(cases.data$variable[x])-2, nchar(cases.data$variable[x])-2) == '_'), 
-          substr(cases.data$variable[x], 1, nchar(cases.data$variable[x])-3), 
-          NA) 
+  ifelse( (substr(cases.data$variable[x], nchar(cases.data$variable[x])-2, nchar(cases.data$variable[x])-2) == '_'),
+          substr(cases.data$variable[x], 1, nchar(cases.data$variable[x])-3),
+          NA)
 })
 
 # Move national deaths to its own column and then drop the (now) unnecessary rows and 'variable' column
@@ -94,7 +94,7 @@ cases.data <- cases.data %>%
 # Re-insert missing values where subnational data was missing
 cases.data$cum_confirmed_cases[ is.na(cases.data$new_confirmed_cases) ] <- NA
 
-# Do the cumulative sum, imputing new cases at the adm2 level for the days in which it was not 
+# Do the cumulative sum, imputing new cases at the adm2 level for the days in which it was not
 # reported (3/2/20 and 3/2/20). Save as a separate set of columns with "_imputed" names.
 cases.data$new_confirmed_cases_imputed <- cases.data$new_confirmed_cases
 
@@ -103,7 +103,7 @@ weights <- weights / sum(weights)
 
 for (d in missing.dates) {
   aggregate_to_distribute <- cases.data$new_confirmed_cases[ (as.character(cases.data$date)==d) & is.na(cases.data$adm1_name) ]
-  
+
   cases.data$new_confirmed_cases_imputed[ (as.character(cases.data$date)==d) & is.na(cases.data$new_deaths_national) ] <- round( weights * aggregate_to_distribute)
 }
 
@@ -122,10 +122,10 @@ cases.data$date <- as.Date(cases.data$date, '%m/%d/%Y')
 ### Load and merge the policies
 policies.data <- read.csv(policies_in)
 
-policies.data.end <- min(which(policies.data$date==''))
-policies.data <- policies.data[1:policies.data.end-1,]
-policy.types <- unique(as.character(policies.data$Policy.type))
-policies.data$date <- as.Date(policies.data$date, '%m/%d/%Y')
+# policies.data.end <- min(which(policies.data$date_start==''))
+# policies.data <- policies.data[1:policies.data.end-1,]
+policy.types <- unique(as.character(policies.data$policy))
+policies.data$date <- as.Date(policies.data$date_start, '%m/%d/%Y')
 
 tmp <- as.data.frame(matrix(0,ncol=length(policy.types),nrow=dim(cases.data)[1]))
 colnames(tmp) <- policy.types
@@ -134,38 +134,36 @@ cases.data <- cbind(cases.data, tmp)
 
 
 for (i in 1:dim(policies.data)[1]) {
-  
-  p <- as.character(policies.data$Policy.type[i])
+
+  p <- as.character(policies.data$policy[i])
   d <- policies.data$date[i]
-  
-  if(policies.data$Locations.affected[i]=='National') {
-    
+
+  if(policies.data$adm1_name[i]=='All') {
+
     cases.data[ cases.data$date >= d, p ] <- 1
-  
+
   } else {
-    
-    locations.list <- strsplit(as.character(policies.data$Locations.affected[i]), ', ')[[1]]
+
+    locations.list <- strsplit(as.character(policies.data$adm1_name[i]), ', ')[[1]]
     my_filter <- (as.character(cases.data$adm2_name) %in% locations.list) & (cases.data$date >= d)
     cases.data[ my_filter, p ] <- 1
-    
+
   }
-  
+
 }
 
-cases.data <- cases.data[c('date', 'adm0_name', 'adm1_name', 'adm2_name', policy.types, 'new_confirmed_cases', 'cum_confirmed_cases', 
-                           'new_confirmed_cases_imputed', 'cum_confirmed_cases_imputed', 
+cases.data <- cases.data[c('date', 'adm0_name', 'adm1_name', 'adm2_name', policy.types, 'new_confirmed_cases', 'cum_confirmed_cases',
+                           'new_confirmed_cases_imputed', 'cum_confirmed_cases_imputed',
                            'new_deaths_national', 'cum_deaths')]
 
 # Split out adm2 and adm0 data, and write to .csv
-cases.data.adm2 <- cases.data[ !is.na(cases.data$adm2_name), 
-                               c('date', 'adm0_name', 'adm1_name', 'adm2_name', policy.types, 'new_confirmed_cases', 'cum_confirmed_cases', 
+cases.data.adm2 <- cases.data[ !is.na(cases.data$adm2_name),
+                               c('date', 'adm0_name', 'adm1_name', 'adm2_name', policy.types, 'new_confirmed_cases', 'cum_confirmed_cases',
                                  'new_confirmed_cases_imputed', 'cum_confirmed_cases_imputed')]
 
-cases.data.adm0 <- cases.data[ is.na(cases.data$adm2_name), 
-                               c('date', 'adm0_name', policy.types, 'new_confirmed_cases', 'cum_confirmed_cases', 
+cases.data.adm0 <- cases.data[ is.na(cases.data$adm2_name),
+                               c('date', 'adm0_name', policy.types, 'new_confirmed_cases', 'cum_confirmed_cases',
                                  'new_deaths_national', 'cum_deaths')]
 
 write.csv(cases.data.adm2, file_out, row.names=FALSE)
 write.csv(cases.data.adm0, file_out_adm0, row.names=FALSE)
-
-
