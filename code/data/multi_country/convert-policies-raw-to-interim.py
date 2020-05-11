1817c3947df52cda2dd54ba1814cbce80ab6e9ee
Daniel Allen <danielpaulallen@gmail.com>
2020-05-11 11:11:13 -0700

    implement usa intensity-group implies

--- code/data/multi_country/convert-policies-raw-to-interim.py
+++ code/data/multi_country/convert-policies-raw-to-interim.py
@@ -1,21 +1,28 @@
 import pandas as pd
+import numpy as np
 import src.utils as cutil
-from IPython.display import display as d
 import json
 import operator
 import argparse
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument(
-    "--c",
-    choices=sorted(cutil.ISOS),
-    default=None,
-    type=str.upper,
-    help="Country ISO code",
-)
+def policy_conversion_parser():
+    parser = argparse.ArgumentParser()
 
-parser.add_argument("--l", default=False, action="store_true", help="Print logs")
+    parser.add_argument(
+        "--c",
+        choices=sorted(cutil.ISOS),
+        default=None,
+        type=str.upper,
+        help="Country ISO code",
+    )
+
+    parser.add_argument("--l", default=False, action="store_true", help="Print logs")
+
+    return parser
+
+
+parser = policy_conversion_parser()
 
 args = parser.parse_args()
 print_logs = args.l
@@ -71,14 +78,40 @@ def apply_rule(df, src_policy, op_str, src_val, dst_rule, country_code):
     return df
 
 
+def apply_usa_rule(df, src_policy, dst_policies):
+    src_category, src_group = src_policy.split(".")
+
+    intensity_cols = [c for c in df.columns if c.startswith("intensity_group")]
+    src_category_mask = df["policy"] == src_category
+
+    psrc = df[src_category_mask].copy()
+
+    src_group_mask = np.zeros_like(psrc.columns[0], dtype=bool)
+    for c in intensity_cols:
+        src_group_mask = (src_group_mask) | (psrc[c] == src_group)
+
+    psrc = psrc[src_group_mask].copy()
+
+    for dst_policy in dst_policies:
+        dst_category, dst_group = dst_policy.split(".")
+        pcopy = psrc.copy()
+        pcopy["policy"] = dst_category
+        pcopy[intensity_cols[0]] = dst_group
+        df = pd.concat([df, pcopy], ignore_index=True)
+
+    return df
+
+
 def apply_implies(df, implies, country_code):
     for rule in implies:
-        # Parse a single rule
-        src_policy, op_str, src_val, dst_rules_list = rule
+        if country_code == "USA":
+            df = apply_usa_rule(df, rule, implies[rule])
+        else:
+            src_policy, op_str, src_val, dst_rules_list = rule
 
-        # For each destination policy, set the appropriate value based on the source
-        for dst_rule in dst_rules_list:
-            df = apply_rule(df, src_policy, op_str, src_val, dst_rule, country_code)
+            # For each destination policy, set the appropriate value based on the source
+            for dst_rule in dst_rules_list:
+                df = apply_rule(df, src_policy, op_str, src_val, dst_rule, country_code)
 
     return df
 
@@ -98,8 +131,6 @@ def process_country(country_code, implies):
     df = pd.read_csv(path_raw, encoding="latin1")
     if country_code not in implies:
         print(f"missing country: {country_code}")
-    elif country_code == "IRN":
-        print("Skipping IRN for now...")
     else:
         print(country_code)
         df = apply_implies(df, implies[country_code], country_code)
