99ac2447a9147582a99e7bda1b584a11f5f0b2c4
Daniel Allen <danielpaulallen@gmail.com>
2020-05-11 23:57:06 -0700

    speed up merge

--- code/src/merge.py
+++ code/src/merge.py
@@ -369,7 +369,15 @@ def calculate_intensities_adm_day_policy(
 
 
 def get_policy_vals(
-    policies, policy, date, adm, adm1, adm_level, policy_pickle_dict, method="ITA"
+    policies,
+    policy,
+    date,
+    adm,
+    adm1,
+    adm_level,
+    policy_pickle_dict,
+    policies_to_date_cache,
+    method="ITA",
 ):
     """Assign all policy variables from `policies` to `cases_df`
     Args:
@@ -397,16 +405,7 @@ def get_policy_vals(
         ]
     )
 
-    policies_to_date = policies[
-        (policies["policy"] == policy)
-        & (policies["date_start"] <= date)
-        & (policies["date_end"] >= date)
-        & ((policies[adm_name] == adm) | (policies[adm_name].str.lower() == "all"))
-        & (
-            (policies["adm1_name"] == adm1)
-            | (policies["adm1_name"].str.lower() == "all")
-        )
-    ].copy()
+    policies_to_date = policies_to_date_cache[adm][policy][str(date)[:10]]
 
     if len(policies_to_date) == 0:
         return (0, 0, 0, 0)
@@ -456,6 +455,69 @@ def initialize_panel(cases_df, cases_level, policy_list, policy_popwts):
         policy_panel.insert(1, "adm1_name", adm1s)
 
     return policy_panel
+
+
+def cached_state_group(policies, adm1, policy_group, empty_df, adm_level, adm2, dates):
+    cached_policies = dict()
+
+    mask = (policies["policy"] == policy_group) & (
+        policies["adm1_name"].isin(["All", "all", adm1])
+    )
+    if adm_level == 2:
+        mask = (mask) & (policies[f"adm2_name"].isin(["All", "all", adm2]))
+
+    policies_in_group = policies[mask].reset_index(drop=True)
+
+    policies_in_group["next_day"] = np.roll(policies_in_group["date_start"], -1)
+
+    for i, row in policies_in_group.iterrows():
+        cache = policies_in_group[: i + 1]
+        cached_policies[row["date_str"]] = cache
+        if row["date_start"] != row["next_day"]:
+            # Return a new version to set the current one in stone
+            cache = cache.copy()
+
+    cache = empty_df.copy()
+    for date in dates:
+        if date in cached_policies:
+            cache = cached_policies[date]
+        else:
+            cached_policies[date] = cache
+
+    return cached_policies
+
+
+def get_policies_to_date_cache(policies, policy_panel, adm_level):
+    dates = policy_panel["date"].astype(str).unique()
+    if adm_level == 2:
+        adm2_to_adm1 = policy_panel.set_index("adm2_name")["adm1_name"].to_dict()
+
+    adms = policy_panel[f"adm{adm_level}_name"].unique()
+    policy_list = policies["policy"].unique()
+
+    cached_policies_on_date = dict()
+    for adm in adms:
+        cached_policies_on_date[adm] = dict()
+        for policy in policy_list:
+            cached_policies_on_date[adm][policy] = dict()
+
+    policies["date_str"] = policies["date_start"].astype(str)
+
+    empty_df = pd.DataFrame(columns=policies.columns)
+    for adm in adms:
+        if adm_level == 2:
+            adm1 = adm2_to_adm1[adm]
+            adm2 = adm
+        else:
+            adm1 = adm
+            adm2 = None
+
+        for policy_group in policy_list:
+            cached_policies_on_date[adm][policy_group] = cached_state_group(
+                policies, adm1, policy_group, empty_df, adm_level, adm2, dates
+            )
+
+    return cached_policies_on_date
 
 
 def assign_policies_to_panel(
@@ -533,6 +595,10 @@ def assign_policies_to_panel(
 
     policy_panel = initialize_panel(cases_df, cases_level, policy_list, policy_popwts)
 
+    policies_to_date_cache = get_policies_to_date_cache(
+        policies, policy_panel, cases_level
+    )
+
     # Assign each policy one-by-one to the panel
     for policy in policy_list:
         policy_pickle_dict = dict()
@@ -548,6 +614,7 @@ def assign_policies_to_panel(
                 row[f"adm1_name"],
                 cases_level,
                 policy_pickle_dict,
+                policies_to_date_cache,
                 method,
             ),
             axis=1,
