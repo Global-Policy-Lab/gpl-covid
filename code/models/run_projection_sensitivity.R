1898f60b891de7e0a59179856a12a634372401e7
Kendon Bell <kmb56@berkeley.edu>
2020-05-12 19:14:14 +1200

    update projection code

--- code/models/run_projection_sensitivity.R
+++ code/models/run_projection_sensitivity.R
@@ -1,4 +1,5 @@
 suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(gt))
 suppressPackageStartupMessages(library(ggsci))
 suppressPackageStartupMessages(library(patchwork))
 
@@ -14,13 +15,24 @@ gamma_estimates <- read_csv("models/gamma_est.csv",
                               recovery_delay = col_double(),
                               gamma = col_double()
                             ))
+
 gamma_estimates <- gamma_estimates %>% 
   filter(adm0_name %in% c("CHN", "KOR")) %>% 
   group_by(recovery_delay) %>% 
   summarise(gamma = mean(gamma))
 
-gamma_loop_df <- crossing(
+underreporting <- read_csv("data/interim/multi_country/under_reporting.csv",
+                           col_types = cols(
+                             country = col_character(),
+                             underreporting_estimate = col_double()
+                           ))
+
+paper_gamma = gamma
+paper_ifr = 0.0075
+
+loop_df <- crossing(
   tibble(country =   c("china", "iran", "korea", "italy", "france", "usa"),
+         display_country = c("China", "Iran", "South Korea", "Italy", "France", "United States"),
          urcountry = c("China", "Iran", "South Korea", "Italy", "France", "United States of America"),
          full_data = list(china_data, iran_data, korea_data, italy_data, france_data, usa_data),
          policy_variables_to_use = list(china_policy_variables_to_use, iran_policy_variables_to_use, 
@@ -33,13 +45,16 @@ gamma_loop_df <- crossing(
                  "D_l_cum_confirmed_cases", "D_l_cum_confirmed_cases", "D_l_cum_confirmed_cases"),
          model = list(china_model, iran_model, korea_model, italy_model, france_model, usa_model)
   ),
-  gamma = c(0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4),
+  gamma = unique(c(0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, paper_gamma)),
   sigma = c(0.2, 0.33, 0.5, Inf),
   time_steps_per_day = c(6)
 ) %>% 
-  left_join(underreporting, by = c("urcountry" = "country"))
-pb <- dplyr::progress_estimated(nrow(gamma_loop_df))
-gamma_loop_df <- gamma_loop_df %>% 
+  left_join(underreporting, by = c("urcountry" = "country")) %>% 
+  filter(ifr == paper_ifr | gamma == paper_gamma)
+
+pb <- dplyr::progress_estimated(nrow(loop_df))
+
+loop_df <- loop_df %>% 
   mutate(projection = list(full_data, model, 
                            lhs, policy_variables_to_use, 
                            other_control_variables,
@@ -60,7 +75,8 @@ gamma_loop_df <- gamma_loop_df %>%
              out
            }))
 
-final_df <- gamma_loop_df %>% 
+gamma_df <- loop_df %>% 
+  filter(ifr == paper_ifr, gamma %in% c(0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4)) %>% 
   select(country, gamma, sigma, time_steps_per_day, projection) %>% 
   unnest(projection) %>% 
   group_by(country, gamma, sigma, time_steps_per_day) %>% 
@@ -73,7 +89,7 @@ final_df <- gamma_loop_df %>%
   ungroup() %>% 
   mutate(sigma = as.character(sigma))
 
-out_scale_down_to_zero <- final_df %>%
+out_scale_down_to_zero <- gamma_df %>%
   ggplot() + 
   aes(x = gamma, y = cases_saved, color = factor(sigma), shape = factor(sigma)) + 
   geom_point() + 
@@ -89,7 +105,7 @@ out_scale_down_to_zero <- out_scale_down_to_zero +
                 limits = c(10, 10^8), 
                 expand = c(0, 0))
 
-out_scale_trimmed <- final_df %>% 
+out_scale_trimmed <- gamma_df %>% 
   ggplot() + 
   aes(x = gamma, y = cases_saved, color = factor(sigma), shape = factor(sigma)) + 
   geom_point() + 
@@ -103,7 +119,7 @@ out_scale_trimmed <- out_scale_trimmed +
   scale_y_continuous("Confirmed cases delayed\n(linear scale trimmed)", labels = scales::comma, 
                      limits = yr_2, expand = c(0, 0))
 
-out_scale_trimmed_true <- final_df %>% 
+out_scale_trimmed_true <- gamma_df %>% 
   ggplot() + 
   aes(x = gamma, y = predicted_cum_confirmed_cases_true, color = factor(sigma), shape = factor(sigma)) + 
   geom_point() + 
@@ -121,7 +137,7 @@ suppressWarnings({
                        limits = yr_3, expand = expand_scale(c(0, 0)))
 })
 
-out_scale_trimmed_no_policy <- final_df %>% 
+out_scale_trimmed_no_policy <- gamma_df %>% 
   ggplot() + 
   aes(x = gamma, y = predicted_cum_confirmed_cases_no_policy, color = factor(sigma), shape = factor(sigma)) + 
   geom_point() + 
@@ -156,8 +172,78 @@ suppressWarnings({
                      scale = 2, base_asp = 1.2)
   cowplot::save_plot(plot = final, filename = "results/figures/fig4/fig4_total_sensitivity_to_gamma.jpg",
                      scale = 2, base_asp = 1.2, dpi = 600)
-  write_csv(final_df %>% dplyr::select(-time_steps_per_day), path = "results/source_data/ExtendedDataFigure7.csv")
+  write_csv(gamma_df %>% dplyr::select(-time_steps_per_day), path = "results/source_data/ExtendedDataFigure7.csv")
   if(file.exists("Rplots.pdf")){
     file.remove("Rplots.pdf")
   }
 })
+
+ifr_df <- loop_df %>%
+  filter(gamma == paper_gamma, sigma == Inf) %>% 
+  select(ifr, display_country, projection, underreporting_estimate) %>% 
+  unnest(projection) %>% 
+  group_by(ifr, display_country) %>% 
+  filter(date == max(date)) %>% 
+  ungroup() %>% 
+  mutate(cases_saved = predicted_cum_confirmed_cases_no_policy - 
+           predicted_cum_confirmed_cases_true) %>% 
+  mutate(infections_saved = cases_saved / underreporting_estimate) %>% 
+  arrange(ifr, display_country) %>%
+  select(ifr, display_country, cases_saved, infections_saved) %>% 
+  mutate(cases_saved = round(cases_saved, -3)) %>% 
+  mutate(infections_saved = round(infections_saved, -3))
+  
+out <- ifr_df %>% 
+  pivot_wider(names_from = ifr,
+              values_from = c(cases_saved, infections_saved)) %>% 
+  gt(rowname_col = "display_country") %>% 
+  tab_header(title = html("Supplementary Table 6 | Sensitivity of cases and infections<br>delayed/avoided to assumed infection fatality rate")) %>% 
+  tab_spanner(label = c("IFR = 0.5%"),
+              columns = vars(cases_saved_0.005, infections_saved_0.005)) %>% 
+  tab_spanner(label = c("IFR = 0.75% (Main results)"),
+              columns = vars(cases_saved_0.0075, infections_saved_0.0075)) %>% 
+  tab_spanner(label = c("IFR = 1.0%"),
+              columns = vars(cases_saved_0.01, infections_saved_0.01)) %>% 
+  cols_label(cases_saved_0.005 = html("Confirmed cases<br>avoided/delayed"),
+             infections_saved_0.005 = html("Infections<br>avoided/delayed"),
+             cases_saved_0.0075 = html("Confirmed cases<br>avoided/delayed"),
+             infections_saved_0.0075 = html("Infections<br>avoided/delayed"),
+             cases_saved_0.01 = html("Confirmed cases<br>avoided/delayed"),
+             infections_saved_0.01 = html("Infections<br>avoided/delayed")) %>%  
+  cols_align("right") %>% 
+  grand_summary_rows(
+    fns = list(
+      Total = ~sum(., na.rm = TRUE)
+    ),
+    decimals = 0
+  ) %>% 
+  fmt_number(columns = everything(),
+             decimals = 0) %>% 
+  tab_options(table.border.top.width = 0,
+              column_labels.border.top.width = 0,
+              heading.border.bottom.color = "black",
+              column_labels.border.bottom.color = "black",
+              stub.border.width = 0,
+              table_body.hlines.width = 0,
+              table_body.border.bottom.color = "black",
+              grand_summary_row.border.color = "black",
+              grand_summary_row.border.style = "single",
+              grand_summary_row.border.width = 2,
+              ) %>% 
+  tab_style(style = cell_text(color = "black", font = "Arial"),
+            locations = list(cells_column_labels(everything()),
+                             cells_column_spanners(everything()),
+                             cells_body(),
+                             cells_stub(),
+                             cells_grand_summary(),
+                             cells_title("title"))) %>% 
+  tab_style(style = cell_text(weight = "bold", size = px(11/0.75), color = "black",
+                              align = "left"), 
+            locations = cells_title("title")); out
+
+owd <- getwd()
+dir.create("results/tables/table_a6/", showWarnings = FALSE)
+setwd("results/tables/table_a6")
+# There's a bug in gt that makes you hard code the wd
+gt::gtsave(out, filename = "sensitivity_to_ifr.html")
+setwd(owd)
